99d306eeddb657a564496c6884563b5cd8a19873
Umme-Nahida <mysuccessmission@gmail.com>
2024-06-28 15:47:25 +0600

    added banner content

--- src/Home-component/Services/Service.jsx
+++ src/Home-component/Services/Service.jsx
@@ -24,7 +24,7 @@ const Service = () => {
           <p>
             the majority have suffered alteration in some form, by injected
             humour, or randomised words which don&apos;t look even slightly
-            believable.{" "}
+            believable
           </p>
         </div>
 
